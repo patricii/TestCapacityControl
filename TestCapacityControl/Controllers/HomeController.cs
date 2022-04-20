b88eb878ce11134496c288c19780459ecfd80e5c
jagapatr <jagapatr@motorola.com>
2022-04-20 12:54:12 -0300

    Bootstrap theme changed

--- TestCapacityControl/Controllers/HomeController.cs
+++ TestCapacityControl/Controllers/HomeController.cs
@@ -17,14 +17,14 @@ namespace TestCapacityControl.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "Test Capacity Control by Product.";
 
             return View();
         }
 
         public IActionResult Contact()
         {
-            ViewData["Message"] = "Your contact page.";
+            ViewData["Message"] = "Test Capacity Control Web Page.";
 
             return View();
         }
