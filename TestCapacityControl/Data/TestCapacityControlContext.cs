f017320c6e6d8e994edf04c3e0afd2c7e16dd781
jagapatr <jagapatr@motorola.com>
2022-04-20 12:02:16 -0300

    First Migration and DB mysql created

---
+++ TestCapacityControl/Data/TestCapacityControlContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace TestCapacityControl.Models
+{
+    public class TestCapacityControlContext : DbContext
+    {
+        public TestCapacityControlContext (DbContextOptions<TestCapacityControlContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<TestCapacityControl.Models.Department> Departament { get; set; }
+    }
+}
---
+++ TestCapacityControl/Models/Department.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TestCapacityControl.Models
+{
+    public class Department
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string JobTitle { get; set; }
+        public string Email { get; set; }
+    }
+}
