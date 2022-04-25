39056a11daff778884becd37411164f7f409f601
jagapatr <jagapatr@motorola.com>
2022-04-25 11:27:59 -0300

    Yield properties added to product class

--- TestCapacityControl/Models/Products.cs
+++ TestCapacityControl/Models/Products.cs
@@ -12,6 +12,24 @@ namespace TestCapacityControl.Models
         public string Model { get; set; }
         public string Status { get; set; }
 
-        public double Yield = 98.5;
+        public double YieldInline = 0.0;
+        public double YieldBrdTest = 0.0;
+        public double YieldBoardTst = 0.0;
+        public double Yield5GFR = 0.0;
+        public double YieldDCAL = 0.0;
+        public double YieldDVAL = 0.0;
+        public double YieldCamCAL = 0.0;
+        public double YieldCamVAL = 0.0;
+        public double YieldJOT = 0.0;
+        public double YieldL2Vision = 0.0;
+        public double YieldL2AR= 0.0;
+        public double YieldM2Cam = 0.0;
+        public double YieldM2AR = 0.0;
+        public double YieldJotSlim = 0.0;
+        public double YieldARSlim = 0.0;
+        public double YieldCFC = 0.0;
+        public double YieldPatsJot = 0.0;
+        public double YieldPatsM2 = 0.0;
+        public double YieldPatsSlim= 0.0;
     }
 }
