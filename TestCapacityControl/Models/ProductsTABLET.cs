629eab8c7b5e274ce9e92400b26fc116bb4663f0
jagapatr <jagapatr@motorola.com>
2022-04-25 14:32:17 -0300

    Yield properties added to productTABLET class

--- TestCapacityControl/Models/ProductsTABLET.cs
+++ TestCapacityControl/Models/ProductsTABLET.cs
@@ -11,5 +11,21 @@ namespace TestCapacityControl.Models
         public string ProductName { get; set; }
         public string Model { get; set; }
         public string Status { get; set; }
+
+
+        public double YieldInline = 0.0;
+        public double YieldWifi = 0.0;
+        public double YieldRFCal = 0.0;
+        public double YieldNSFT = 0.0;
+        public double YieldRadCurr = 0.0;
+        public double YieldTOF = 0.0;
+        public double YieldANT = 0.0;
+        public double YieldMMI1 = 0.0;
+        public double YieldMMI2 = 0.0;
+        public double YieldAUDIO = 0.0;
+        public double YieldWriteNumber = 0.0;
+        public double YieldCFCUp = 0.0;
+        public double YieldMMI3 = 0.0;
+
     }
 }
