3eeda6636c7a06a9eb17b4a55e318fb6a551cf2b
jagapatr <jagapatr@motorola.com>
2022-04-26 14:51:32 -0300

    Added new properties to Products and ProductsTablet Classes

--- TestCapacityControl/Models/Products.cs
+++ TestCapacityControl/Models/Products.cs
@@ -31,5 +31,45 @@ namespace TestCapacityControl.Models
         public double YieldPatsJot = 0.0;
         public double YieldPatsM2 = 0.0;
         public double YieldPatsSlim= 0.0;
+
+        public double NtfInline = 0.0;
+        public double NtfBrdTest = 0.0;
+        public double NtfBoardTst = 0.0;
+        public double Ntf5GFR = 0.0;
+        public double NtfDCAL = 0.0;
+        public double NtfDVAL = 0.0;
+        public double NtfCamCAL = 0.0;
+        public double NtfCamVAL = 0.0;
+        public double NtfJOT = 0.0;
+        public double NtfL2Vision = 0.0;
+        public double NtfL2AR = 0.0;
+        public double NtfM2Cam = 0.0;
+        public double NtfM2AR = 0.0;
+        public double NtfJotSlim = 0.0;
+        public double NtfARSlim = 0.0;
+        public double NtfCFC = 0.0;
+        public double NtfPatsJot = 0.0;
+        public double NtfPatsM2 = 0.0;
+        public double NtfPatsSlim = 0.0;
+
+        public double MTimeInline = 0.0;
+        public double MTimeBrdTest = 0.0;
+        public double MTimeBoardTst = 0.0;
+        public double MTime5GFR = 0.0;
+        public double MTimeDCAL = 0.0;
+        public double MTimeDVAL = 0.0;
+        public double MTimeCamCAL = 0.0;
+        public double MTimeCamVAL = 0.0;
+        public double MTimeJOT = 0.0;
+        public double MTimeL2Vision = 0.0;
+        public double MTimeL2AR = 0.0;
+        public double MTimeM2Cam = 0.0;
+        public double MTimeM2AR = 0.0;
+        public double MTimeJotSlim = 0.0;
+        public double MTimeARSlim = 0.0;
+        public double MTimeCFC = 0.0;
+        public double MTimePatsJot = 0.0;
+        public double MTimePatsM2 = 0.0;
+        public double MTimePatsSlim = 0.0;
     }
 }
--- TestCapacityControl/Models/ProductsTABLET.cs
+++ TestCapacityControl/Models/ProductsTABLET.cs
@@ -27,5 +27,33 @@ namespace TestCapacityControl.Models
         public double YieldCFCUp = 0.0;
         public double YieldMMI3 = 0.0;
 
+        public double NTFInline = 0.0;
+        public double NTFWifi = 0.0;
+        public double NTFRFCal = 0.0;
+        public double NTFNSFT = 0.0;
+        public double NTFRadCurr = 0.0;
+        public double NTFTOF = 0.0;
+        public double NTFANT = 0.0;
+        public double NTFMMI1 = 0.0;
+        public double NTFMMI2 = 0.0;
+        public double NTFAUDIO = 0.0;
+        public double NTFWriteNumber = 0.0;
+        public double NTFCFCUp = 0.0;
+        public double NTFMMI3 = 0.0;
+
+        public double MTimeInline = 0.0;
+        public double MTimeWifi = 0.0;
+        public double MTimeRFCal = 0.0;
+        public double MTimeNSFT = 0.0;
+        public double MTimeRadCurr = 0.0;
+        public double MTimeTOF = 0.0;
+        public double MTimeANT = 0.0;
+        public double MTimeMMI1 = 0.0;
+        public double MTimeMMI2 = 0.0;
+        public double MTimeAUDIO = 0.0;
+        public double MTimeWriteNumber = 0.0;
+        public double MTimeCFCUp = 0.0;
+        public double MTimeMMI3 = 0.0;
+
     }
 }
